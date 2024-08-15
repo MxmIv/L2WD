39a2960a8ea8ea4e7bcd3797230f03a6f2397672
Max <mxmivlev@gmail.com>
2024-08-15 22:10:25 +0100

    on hover brown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
@@ -5,31 +6,29 @@ module.exports = {
     content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
-            backgroundImage: {
-                'grid-pattern': "linear-gradient(to bottom, theme('colors.neutral.950 / 0%'), theme('colors.neutral.950 / 100%')), url('/images/noise.png')"
-            },
             colors: {
+                customBrown: '#693D14', // Add the custom brown color
                 neutral: colors.neutral,
-                primary: '#2bdcd2',  // Updated primary color
+                primary: '#2bdcd2',
             },
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
-            }
-        }
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     daisyui: {
         themes: [
             {
                 lofi: {
                     ...require('daisyui/src/theming/themes')['lofi'],
-                    primary: '#2bdcd2', // Updated primary color
+                    primary: '#2bdcd2',
                     'primary-content': '#171717',
                     secondary: '#016968',
                     info: '#2bdcd2',
                     'info-content': '#171717',
-                }
-            }
-        ]
+                },
+            },
+        ],
     },
-    plugins: [require('daisyui')]
+    plugins: [require('daisyui')],
 };
