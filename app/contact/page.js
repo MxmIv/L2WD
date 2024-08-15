ce921f738dcaf9789b78faa61d17033c57290fca
Max <mxmivlev@gmail.com>
2024-08-15 21:57:30 +0100

    basic contact us page

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,14 +1,11 @@
 // app/contact/page.js
 
-import ContactUs from '../../components/ContactUs'; // Adjust the import path as needed
+import ContactUs from '../../components/ContactUs'; // Import the ContactUs component
 
 export default function ContactPage() {
     return (
         <div className="container mx-auto px-4 py-20">
             <h1 className="text-4xl font-bold mb-8">Contact Us</h1>
-            <p className="text-lg mb-6">
-                We would love to hear from you! Whether you have questions about our wedding dance lessons, private lessons, hen parties, or anything else, our team is ready to answer all your questions.
-            </p>
 
             {/* Reusing the ContactUs component */}
             <ContactUs />
