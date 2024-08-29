fb779b6821d80575459798b6c44fbbc0c55998d1
Max <mxmivlev@gmail.com>
2024-08-29 18:57:12 +0100

    Add gtag (testing)

--- app/layout.jsx
+++ app/layout.jsx
@@ -41,6 +41,18 @@ export default function RootLayout({ children }) {
             <link rel="icon" href="/L2WD_logo.svg" sizes="any" />
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
             <title>{metadata.title.default}</title>
+            {/* Google Analytics */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-HXE6NMYXM8"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-HXE6NMYXM8');
+                        `,
+                }}
+            />
         </head>
         <body className="antialiased text-gray-900 bg-white pt-20">
         <div className="flex flex-col min-h-screen">
